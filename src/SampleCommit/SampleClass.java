cae610dc29aa0d7dfe1af511c0c208cdfacc60b5
PCS <PCS@DESKTOP-2PJ77R5>
2022-07-25 17:17:47 +0530

    SampleClass commit

---
+++ src/SampleCommit/SampleClass.java
@@ -0,0 +1,11 @@
+package SampleCommit;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Jyoti has created samplecls");
+	}
+
+}
